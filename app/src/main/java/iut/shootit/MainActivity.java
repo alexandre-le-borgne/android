4329914ea37fb68a06e226e5d02b112f5c5b6afc
JJTheo <theo.jeanjean42@gmail.com>
2016-03-25 09:45:01 +0100

    oizjdoazjdaozidazdazaz

--- app/src/main/java/iut/shootit/MainActivity.java
+++ app/src/main/java/iut/shootit/MainActivity.java
@@ -1,29 +1,27 @@
 package iut.shootit;
 
-import android.support.design.widget.TabLayout;
+import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-   
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-  
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     /**
      * The {@link android.support.v4.view.PagerAdapter} that will provide
-     * fragments for each of the sections. We use as
+     * fragments for each of the sections. We use a
      * {@link FragmentPagerAdapter} derivative, which will keep every
      * loaded fragment in memory. If this becomes too memory intensive, it
      * may be best to switch to a
@@ -62,7 +60,7 @@ public class MainActivity extends AppCompatActivity {
                       .setAction("Action", null).show();
           }
       });
-      
+
     }
 
 
@@ -88,8 +86,8 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    
-  
+
+
 
     /**
      * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
