39f107421a3816b8acfe23c7d71683061beb2ed5
jjtheo <theo.jeanjean42@gmail.com>
2016-04-07 02:42:01 +0200

    adzazdazdœ

--- app/src/main/java/iut/shootit/fragments/MainPhotoFragment.java
+++ app/src/main/java/iut/shootit/fragments/MainPhotoFragment.java
@@ -1,9 +1,11 @@
 package iut.shootit.fragments;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.media.Image;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.support.v4.app.Fragment;
@@ -31,6 +33,7 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 
 import iut.shootit.CaptureActivity;
+import iut.shootit.MainActivity;
 import iut.shootit.R;
 
 /**
@@ -40,7 +43,7 @@ public class MainPhotoFragment extends Fragment {
     private Button takePic;
     private Button seePic;
     private TextView tv;
-
+    private ImageView iv;
 
     private static MainPhotoFragment instance;
 
@@ -60,6 +63,8 @@ public class MainPhotoFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.main_photo_fragment,
                 container, false);
+
+        //Prendre une photo (new intent)
         takePic = (Button) rootView.findViewById(R.id.take_picture);
         takePic.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -69,6 +74,7 @@ public class MainPhotoFragment extends Fragment {
             }
         });
 
+        //Bouton pour voir une image en fonction d'un lien
         seePic = (Button) rootView.findViewById(R.id.see_picture);
         tv = (EditText) rootView.findViewById(R.id.choice_text);
         final String imageId = tv.getText().toString();
@@ -84,45 +90,88 @@ public class MainPhotoFragment extends Fragment {
     }
 
     private void loadImg(String imageId) {
-        String url = CaptureActivity.URL;
-        JSONObject retour;
+        new DownloadJSON(imageId, this).execute();
 
-        ArrayList<NameValuePair> nameValuePairs = new ArrayList<>();
+    }
 
-        nameValuePairs.add(new BasicNameValuePair("action", "shot"));
-        nameValuePairs.add(new BasicNameValuePair("shot_id", imageId));
 
-        try {
-            HttpClient httpclient = new DefaultHttpClient();
-            HttpPost httppost = new HttpPost(url);
-            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
-            HttpResponse response = httpclient.execute(httppost);
-            String str = EntityUtils.toString(response.getEntity());
-            Log.v("log_tag", "In the try Loop" + str);
+    public class DownloadJSON extends AsyncTask<Void, Void, String> {
 
-            retour = new JSONObject(str);
+//        private ProgressDialog pd = new ProgressDialog(MainPhotoFragment.this);
 
-        } catch (Exception e) {
-            Log.v("log_tag", "Error in http connection ");
-            e.printStackTrace();
-            return;
+        private String imageId;
+        private MainPhotoFragment thisSuper;
+
+        public DownloadJSON(String imageId, MainPhotoFragment thisSuper) {
+            this.imageId = imageId;
+            this.thisSuper = thisSuper;
         }
 
-        String imageBase64;
-        try {
-            imageBase64 = retour.getString("base64");
-        } catch (JSONException e) {
-            e.printStackTrace();
-            return;
+        private ProgressDialog pd = new ProgressDialog(getActivity());
+
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pd.setMessage("Wait image uploading!");
+            pd.show();
         }
 
-        byte[] decodedString = Base64.decode(imageBase64, Base64.DEFAULT);
-        Bitmap image = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+        @Override
+        protected String doInBackground(Void... params) {
+
+            String url = CaptureActivity.URL;
+            JSONObject retour;
+
+            ArrayList<NameValuePair> nameValuePairs = new ArrayList<>();
+
+            nameValuePairs.add(new BasicNameValuePair("action", "shot"));
+            nameValuePairs.add(new BasicNameValuePair("shot_id", imageId));
+
+            try {
+                HttpClient httpclient = new DefaultHttpClient();
+                HttpPost httppost = new HttpPost(url);
+                httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+                HttpResponse response = httpclient.execute(httppost);
+                String str = EntityUtils.toString(response.getEntity());
+                Log.v("log_tag", "In the try Loop" + str);
+
+                retour = new JSONObject(str);
+
+            } catch (Exception e) {
+                Log.v("log_tag", "Error in http connection ");
+                e.printStackTrace();
+                return "error";
+            }
+
+            String imageBase64;
+            try {
+                imageBase64 = retour.getString("base64");
+            } catch (JSONException e) {
+                e.printStackTrace();
+                return "error";
+            }
+
+            byte[] decodedString = Base64.decode(imageBase64, Base64.DEFAULT);
+            Bitmap image = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+
+            thisSuper.afficherImage(image);
+
+
+            return "Success";
+
+        }
+
+
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            pd.hide();
+            pd.dismiss();
+        }
+    }
+
+    private void afficherImage(Bitmap image) {
 
         ImageView imageView = (ImageView) getActivity().findViewById(R.id.image_shot);
         imageView.setImageBitmap(image);
-
-
     }
 
 } // class MainPhotoFragment
